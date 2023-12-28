d613f193856eb8083db1968371e69480471431da
ywateba <59646094+ywateba@users.noreply.github.com>
2023-12-28 12:59:06 +0000

    configure aws

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# video demo - https://www.youtube.com/watch?v=Y8TyE_DNds8
+
+mkdir ~/.tmp && cd $_
+
+# install aws-cli v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+  unzip awscliv2.zip && \
+  sudo ./aws/install
+
+mkdir ~/.bin && cd $_
+wget https://raw.githubusercontent.com/pahud/vscode/main/.devcontainer/bin/aws-sso-credential-process && \
+chmod +x aws-sso-credential-process
+
+
+aws configure set credential_process ${HOME}/.bin/aws-sso-credential-process
+touch ~/.aws/credentials && chmod 600 $_
+#aws configure  --profile default
