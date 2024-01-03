b5572ba019296a3d0e8e659f72294b1b7440f39f
ywateba <59646094+ywateba@users.noreply.github.com>
2024-01-03 00:57:02 +0000

    to check

--- script.sh
+++ script.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
+#set -e
+
 
 STACK=$1
 OPERATION=$2
 
 NETWORK_STACK_NAME="NETWORK"
 UDAGRAM_STACK_NAME="UDAGRAM"
+NETWORK_STACK_PATH="network.yml"
 NETWORK_PARAMETERS_PATH="network-parameters.json"
+NETWORK_OUTPUTS_PATH="network-outputs.json"
+UDAGRAM_STACK_PATH="uda.yml"
 UDAGRAM_PARAMETERS_PATH="udagram-parameters.json"
+UDAGRAM_OUTPUTS_PATH="udagram-outputs.json"
 
 
 
 # Function to check and return stack status - no waiting
 get_stack_status() {
-    local stack_name =$1
-    aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 
+    local stack_name=$1
+    aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2>/dev/null
+    
 }
 
 # Function to check and return stack status with waiting
 check_stack_status() {
-    local stack_name = $1
+    local stack_name=$1
     echo "Checking the status of stack $stack_name..."
     while true; do
         status=$(get_stack_status $stack_name)
+        
         case $status in
         "CREATE_COMPLETE")
             echo "Stack $stack_name created successfully."
@@ -29,70 +37,76 @@ check_stack_status() {
             ;;
         "ROLLBACK_COMPLETE" | "CREATE_FAILED")
             echo "Stack $stack_name creation failed."
-            exit 1
+            break
             ;;
-        *)
-            echo "Waiting for stack $stack_name to be created..."
+        "CREATE_IN_PROGRESS" | "UPDATE_IN_PROGRESS")
+            echo "Creation or update  in progress..."
             sleep 10
             ;;
+        "DELETE_IN_PROGRESS")
+            echo "Delete  in progress..."
+            sleep 10
+            ;;
+
+        *)
+            echo "Stack does not exists or has been deleted"
+            break
+            ;;
         esac
+        
     done
 }
 
 
-# Function to get stack outputs in to a file
+# Function to get stack outputs 
 get_stack_outputs(){
-    local stack_name = $1
-    local output_path = $2
-    aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].Outputs" > $output_path
+    local stack_name=$1
+    aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].Outputs" 2>/dev/null 
 }
 
 
-
-#perform operations on network stack
-handle_network_stack(){
-    local operation = 1$
-    manage_stack $NETWORK_STACK_NAME $operation $NETWORK_STACK_PATH $NETWORK_PARAMETERS_PATH
-    # update network paramters for udagram if necessary
-    if [[ ! "$operation" =~ ^(CREATE|UPDATE)$ ]]; then
-        echo "Exporting network stack outputs to JSON file..."
-        get_stack_outputs $NETWORK_STACK_NAME $UDAGRAM_PARAMETERS_PATH
-    fi
+#format outputs and save in to a file
+format_outputs(){
+    local stack_name=$1
+    local output_path=$2
+    outputs=$(get_stack_outputs $stack_name)
+    echo "$outputs" | jq  '[ .[] | {ParameterKey: .OutputKey, ParameterValue: .OutputValue} ]'  > "$output_path"
 }
 
-# Perform operation on udagram stack - create , update or delete
-# Do nothing if network stack does not exist 
-handle_udagram_stack(operation){
-    local operation = 1$
-
-    # we check if network stack exists
-    status=$(get_stack_status $NETWORK_STACK_NAME)
-    echo "Current status of the network stack $stack_name: $status"
-
-    if [[ ! "$status" =~ ^(CREATE_COMPLETE|UPDATE_COMPLETE)$ ]]; then
-        manage_stack $UDAGRAM_STACK_NAME $operation $UDAGRAM_STACK_PATH $UDAGRAM_PARAMETERS_PATH
-    elif
-        echo "Make network stack available first"
-    fi
-   
-}
 
 # Function to manage a specific stack - create , update or delete
 # w.r.t to stack current status
 manage_stack() {
-    local stack_name = $1
-    local operation = $2
-    local stack_file_path = $3
-    local parameters_file_path = $4
+    local stack_name=$1
+    local operation=$2
+    local stack_file_path=$3
+    local parameters_file_path=$4
+    local output_path=$5
 
+    
     status=$(get_stack_status $stack_name)
-    echo "Current status of the stack $stack_name: $status"
+    if [ -z "$status" ]; then
+        echo "The network stack $stack_name does not exist or has been  deleted." 
+        status="NON_EXISTENT"
+    fi
 
+    if [ "$operation" == "CHECK" ]; then
+        echo "Status of  $stack_name: $status."
+        if [[ "$status" == "CREATE_COMPLETE" ]] || [[ "$status" == "UPDATE_COMPLETE" ]]; then
+            echo "Exporting network stack outputs to JSON file..." 
+            format_outputs $stack_name $output_path
+        fi
+    fi
+    
     case $status in
     "CREATE_COMPLETE" | "UPDATE_COMPLETE")
         if [ "$operation" == "UPDATE" ]; then
             echo "Updating stack $stack_name..."
-            aws cloudformation update-stack --stack-name $stack_name --template-body file://$stack_file_path --parameters file://$parameters_file_path
+            aws cloudformation update-stack --stack-name $stack_name \
+                                            --template-body file://$stack_file_path \
+                                            --parameters file://$parameters_file_path \
+                                            --capabilities CAPABILITY_NAMED_IAM
+
         elif [ "$operation" == "DELETE" ]; then
             echo "Deleting stack $stack_name..."
             aws cloudformation delete-stack --stack-name $stack_name
@@ -103,10 +117,17 @@ manage_stack() {
     "CREATE_IN_PROGRESS" | "UPDATE_IN_PROGRESS" | "DELETE_IN_PROGRESS")
         echo "Stack $stack_name is currently in progress. Please wait."
         ;;
-    "ROLLBACK_COMPLETE" | "CREATE_FAILED" | "")
+    "DELETE_IN_PROGRESS")
+        echo "Stack $stack_name is currently being deleted. Please wait."
+        ;;
+    "ROLLBACK_COMPLETE" | "CREATE_FAILED" | "NON_EXISTENT")
+        echo "$operation"
         if [ "$operation" == "CREATE" ]; then
             echo "Creating stack $stack_name..."
-            aws cloudformation create-stack --stack-name $stack_name --template-body file://$stack_file_path --parameters file://$parameters_file_path
+            aws cloudformation create-stack --stack-name $stack_name \
+                                            --template-body file://$stack_file_path \
+                                            --parameters file://$parameters_file_path \
+                                            --capabilities CAPABILITY_NAMED_IAM
         else
             echo "Stack is in a state that requires manual intervention or does not exist."
         fi
@@ -125,28 +146,92 @@ if [[ $# -ne 2 ]]; then
     exit 1
 fi
 
-if [[ ! "$OPERATION" =~ ^(CREATE|UPDATE|DELETE)$ ]]; then
+if [[ ! "$OPERATION" =~ ^(CREATE|UPDATE|DELETE|CHECK)$ ]]; then
     echo "Invalid choice for Operation"
-    echo "Usage: $0 [NETWORK|UDAGRAM|BOTH] [CREATE|UPDATE|DELETE]"
+    echo "Usage: $0 [NETWORK|UDAGRAM] [CREATE|UPDATE|DELETE|CHECK]"
     exit 1
 fi
 
 
+case $OPERATION in
+    CREATE | UPDATE)
+        if [[ "$STACK" == "$NETWORK_STACK_NAME" ]]; then
+            echo "We are handling network stack"
+            manage_stack "$NETWORK_STACK_NAME" "$OPERATION" "$NETWORK_STACK_PATH" "$NETWORK_PARAMETERS_PATH" "$NETWORK_OUTPUTS_PATH"
 
-case $STACK in
-    NETWORK)
-        handle_network_stack $OPERATION
+            check_stack_status $NETWORK_STACK_NAME
+            status=$(get_stack_status $NETWORK_STACK_NAME)
+            
+            # update network parameters for udagram if necessary
+            if [[ "$status" == "CREATE_COMPLETE" ]] || [[ "$status" == "UPDATE_COMPLETE" ]]; then
+                
+                echo "Exporting network stack outputs to JSON file..."
+                format_outputs $NETWORK_STACK_NAME $NETWORK_OUTPUTS_PATH
+            fi
+        fi
+        if [[ "$STACK" == "$UDAGRAM_STACK_NAME" ]]; then
+            echo "We are handling udagram stack"
+            # we check if network stack exists
+            
+            network_status=$(get_stack_status $NETWORK_STACK_NAME)
+            echo "network status $network_status"
+            if [ -z "$network_status" ]; then
+                echo "The network stack $NETWORK_STACK_NAME does not exist or has been  deleted."
+                echo "Make network stack available first"
+                exit 1
+            fi
+
+            if [[ "$network_status" == "CREATE_COMPLETE" ]] || [[ "$network_status" == "UPDATE_COMPLETE" ]]; then
+                echo "creating the UDAGRAM stack"
+                manage_stack "$UDAGRAM_STACK_NAME" "$OPERATION" "$UDAGRAM_STACK_PATH" "$UDAGRAM_PARAMETERS_PATH" "$UDAGRAM_OUTPUTS_PATH"
+                check_stack_status $UDAGRAM_STACK_NAME
+                echo "Exporting UDAGRAM stack outputs to JSON file..."
+                format_outputs $UDAGRAM_STACK_NAME $UDAGRAM_OUTPUTS_PATH
+            fi
+        fi
+        
         ;;
-    UDAGRAM)
-        handle_udagram_stack $OPERATION
+    DELETE)
+        if [[ "$STACK" == "$UDAGRAM_STACK_NAME" ]]; then
+            echo "We are handling udagram stack"
+            status=$(get_stack_status $UDAGRAM_STACK_NAME)
+            if [[ "$status" == "CREATE_COMPLETE" ]] || [[ "status" == "UPDATE_COMPLETE" ]]; then
+                echo "creating the UDAGRAM stack"
+                manage_stack "$UDAGRAM_STACK_NAME" "$OPERATION" "$UDAGRAM_STACK_PATH" "$UDAGRAM_PARAMETERS_PATH" "$UDAGRAM_OUTPUTS_PATH"
+                
+            fi
+            check_stack_status $UDAGRAM_STACK_NAME
+        fi
+        if [[ "$STACK" == "$NETWORK_STACK_NAME" ]]; then
+            echo "We are deleting the whole infrastructure"
+            echo "We are handling udagram stack"
+            status=$(get_stack_status $UDAGRAM_STACK_NAME)
+            if [[ "$status" == "CREATE_COMPLETE" ]] || [[ "status" == "UPDATE_COMPLETE" ]]; then
+                echo "deleting the UDAGRAM stack"
+                manage_stack "$UDAGRAM_STACK_NAME" "$OPERATION" "$UDAGRAM_STACK_PATH" "$UDAGRAM_PARAMETERS_PATH" "$UDAGRAM_OUTPUTS_PATH"
+                check_stack_status $UDAGRAM_STACK_NAME
+            fi
+            check_stack_status $UDAGRAM_STACK_NAME
+            
+            
+            network_status=$(get_stack_status $NETWORK_STACK_NAME)
+            if [[ "$network_status" == "CREATE_COMPLETE" ]] || [[ "status" == "UPDATE_COMPLETE" ]]; then
+                echo "deleting the NETWORK stack"
+                manage_stack "$NETWORK_STACK_NAME" "$OPERATION" "$NETWORK_STACK_PATH" "$NETWORK_PARAMETERS_PATH" "$NETWORK_OUTPUTS_PATH"
+                
+            fi
+            check_stack_status $NETWORK_STACK_NAME
+        fi
     ;;
-    BOTH)
-        # If BOTH is selected, perform the operation on both NETWORK and UDAGRAM stacks
-        handle_network_stack $OPERATION
-        handle_udagram_stack $OPERATION
-        ;;
+    CHECK)
+        echo "We are checking stack $STACK"
+        check_stack_status $STACK 
+    ;;
+
     *)
-        echo "Invalid stack option. Choose NETWORK, UDAGRAM, or BOTH."
+        echo "Invalid stack option. Choose NETWORK, UDAGRAM."
         exit 1
         ;;
 esac
+
+
